09a50adcb9c8e9c54fe90f3a3d4133c79f053c21
gameshome350 <48838868+gameshome350@users.noreply.github.com>
2019-04-21 16:42:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,40 @@
 ﻿ 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "g!"
  
+ 
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+console.log('         [Wait please .. ]       ')
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame("اunder development", "https://www.twitch.tv/Alpha");
-});
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Logged in as [ ${client.user.tag}! ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[           BOT IS ONLINE         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[        info         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
  
+client.user.setGame("under test", "https://www.twitch.tv/alpha");
+});
+
+
+
  
  
  var prefix = "g!"
